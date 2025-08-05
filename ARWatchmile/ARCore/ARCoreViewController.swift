118fe43fb40015ef7353b1e9f652e67e7eee49d6
서충원 <cwseo@vestellalab.com>
2025-08-05 16:02:29 +0900

    FEAT: ARCore Cloud Anchor 테스트

---
+++ ARWatchmile/ARCore/ARCoreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ARCoreViewController.swift
+//  ARWatchmile
+//
+//  Created by 베스텔라랩 on 8/5/25.
+//
+
+import UIKit
+
+class ARCoreViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
